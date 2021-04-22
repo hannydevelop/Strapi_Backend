867ad310a9d2f8e7310292ce679a1a9ccb391cd7
Ukpai Ugochi <62933980+hannydevelop@users.noreply.github.com>
2021-04-22 13:22:17 +0100

    Add files via upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4115397cd8d40203458b25688bc82b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a251f282-51bf-41d0-97c3-0ea3c6f10553'
+};
